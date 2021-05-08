b64dc0f4e32834a7a70a1be7e50abdd84f8d642f
Clément Trassoudaine <clement.trassoudaine@outlook.com>
2021-05-08 09:19:16 +0200

    Implement Notebook viewer

--- src/modules/previewpane/JupyterNotebookPreviewHandler/JupyterNotebookPreviewHandlerControl.cs
+++ src/modules/previewpane/JupyterNotebookPreviewHandler/JupyterNotebookPreviewHandlerControl.cs
@@ -4,12 +4,18 @@
 
 using System;
 using System.Drawing;
+using System.IO;
 using System.IO.Abstractions;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
 using Common;
+using Jupyscope.Contracts;
+using Jupyscope.Extensions;
+using Jupyscope.Serializers;
 using Markdig;
 using Microsoft.PowerToys.PreviewHandler.JupyterNotebook.Properties;
+using Microsoft.PowerToys.PreviewHandler.JupyterNotebook.Telemetry.Events;
 using Microsoft.PowerToys.Telemetry;
 using PreviewHandlerCommon;
 
@@ -21,9 +27,24 @@ namespace Microsoft.PowerToys.PreviewHandler.JupyterNotebook
     public class JupyterNotebookPreviewHandlerControl : FormHandlerControl
     {
         /// <summary>
+        /// Markdown HTML header.
+        /// </summary>
+        private readonly string htmlHeader = "<!doctype html><style>body{width:100%;margin:0;font-family:-apple-system,BlinkMacSystemFont,\"Segoe UI\",Roboto,\"Helvetica Neue\",Arial,\"Noto Sans\",sans-serif,\"Apple Color Emoji\",\"Segoe UI Emoji\",\"Segoe UI Symbol\",\"Noto Color Emoji\";font-size:1rem;font-weight:400;line-height:1.5;color:#212529;text-align:left;background-color:#fff}.container{padding:5%}body img{max-width:100%;height:auto}body h1,body h2,body h3,body h4,body h5,body h6{margin-top:24px;margin-bottom:16px;font-weight:600;line-height:1.25}body h1,body h2{padding-bottom:.3em;border-bottom:1px solid #eaecef}body{font-family:-apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji}body h3{font-size:1.25em}body h4{font-size:1em}body h5{font-size:.875em}body h6{font-size:.85em;color:#6a737d}pre{font-family:SFMono-Regular,Consolas,Liberation Mono,Menlo,monospace;background-color:#f6f8fa;border-radius:3px;padding:16px;font-size:85%}a{color:#0366d6}strong{font-weight:600}em{font-style:italic}code{padding:.2em .4em;margin:0;font-size:85%;background-color:#f6f8fa;border-radius:3px}hr{border-color:#EEE -moz-use-text-color #FFF;border-style:solid none;border-width:.5px 0;margin:18px 0}table{display:block;width:100%;overflow:auto;border-spacing:0;border-collapse:collapse}tbody{display:table-row-group;vertical-align:middle;border-color:inherit;vertical-align:inherit;border-color:inherit}table tr{background-color:#fff;border-top:1px solid #c6cbd1}tr{display:table-row;vertical-align:inherit;border-color:inherit}table td,table th{padding:6px 13px;border:1px solid #dfe2e5}th{font-weight:600;display:table-cell;vertical-align:inherit;font-weight:bold;text-align:-internal-center}thead{display:table-header-group;vertical-align:middle;border-color:inherit}td{display:table-cell;vertical-align:inherit}code,pre,tt{font-family:SFMono-Regular,Menlo,Monaco,Consolas,\"Liberation Mono\",\"Courier New\",monospace;color:#24292e;overflow-x:auto}pre code{font-size:inherit;color:inherit;word-break:normal}blockquote{background-color:#fff;border-radius:3px;padding:15px;font-size:14px;display:block;margin-block-start:1em;margin-block-end:1em;margin-inline-start:40px;margin-inline-end:40px;padding:0 1em;color:#6a737d;border-left:.25em solid #dfe2e5}</style><body><div class=\"container\">";
+
+        /// <summary>
+        /// Markdown HTML footer.
+        /// </summary>
+        private readonly string htmlFooter = "</div></body></html>";
+
+        /// <summary>
         /// RichTextBox control to display if external images are blocked.
         /// </summary>
         private RichTextBox _infoBar;
+
+        /// <summary>
+        /// Extended Browser Control to display markdown html.
+        /// </summary>
+        private WebBrowserExt _browser;
 
         /// <summary>
         /// True if external image is blocked, false otherwise.
@@ -43,18 +64,66 @@ namespace Microsoft.PowerToys.PreviewHandler.JupyterNotebook
         /// <param name="dataSource">Path to the file.</param>
         public override void DoPreview<T>(T dataSource)
         {
-            _infoBarDisplayed = false;
-
-            InvokeOnControlThread(() =>
+            try
             {
-                Controls.Clear();
-                _infoBarDisplayed = true;
-                _infoBar = GetTextBoxControl(Resources.NotebookNotPreviewedError);
-                Resize += FormResized;
-                Controls.Add(_infoBar);
-            });
+                if (!(dataSource is string filePath))
+                {
+                    throw new ArgumentException($"{nameof(dataSource)} for {nameof(JupyterNotebookPreviewHandler)} must be a string but was a '{typeof(T)}'");
+                }
 
-            base.DoPreview(dataSource);
+                string fileText = File.ReadAllText(filePath);
+                Regex imageTagRegex = new Regex(@"<[ ]*img.*>");
+                if (imageTagRegex.IsMatch(fileText))
+                {
+                    _infoBarDisplayed = true;
+                }
+
+                var notebook = TelescopeConverter.Deserialize<Notebook>(fileText);
+                var notebookContentHTML = notebook.ToHtml().Result;
+                var notebookHTML = $"{htmlHeader}{notebookContentHTML}{htmlFooter}";
+
+                InvokeOnControlThread(() =>
+                {
+                    _browser = new WebBrowserExt
+                    {
+                        DocumentText = notebookHTML,
+                        Dock = DockStyle.Fill,
+                        IsWebBrowserContextMenuEnabled = false,
+                        ScriptErrorsSuppressed = true,
+                        ScrollBarsEnabled = true,
+                        AllowNavigation = false,
+                    };
+                    Controls.Add(_browser);
+
+                    if (_infoBarDisplayed)
+                    {
+                        _infoBar = GetTextBoxControl(Resources.BlockedImageInfoText);
+                        Resize += FormResized;
+                        Controls.Add(_infoBar);
+                    }
+                });
+
+                PowerToysTelemetry.Log.WriteEvent(new JupyterNotebookFilePreviewed());
+            }
+#pragma warning disable CA1031 // Do not catch general exception types
+            catch (Exception ex)
+#pragma warning restore CA1031 // Do not catch general exception types
+            {
+                PowerToysTelemetry.Log.WriteEvent(new JupyterNotebookFilePreviewError { Message = ex.Message });
+
+                InvokeOnControlThread(() =>
+                {
+                    Controls.Clear();
+                    _infoBarDisplayed = true;
+                    _infoBar = GetTextBoxControl(Resources.NotebookNotPreviewedError);
+                    Resize += FormResized;
+                    Controls.Add(_infoBar);
+                });
+            }
+            finally
+            {
+                base.DoPreview(dataSource);
+            }
         }
 
         /// <summary>
